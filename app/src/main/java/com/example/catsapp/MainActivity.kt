fa4f67ca69e6d1c2889ca6ebde538a102a3837da
hyeonahkiki <kkongc7@gmail.com>
2020-05-03 23:26:23 +0900

    [+] adapter 설정

--- app/src/main/java/com/example/catsapp/MainActivity.kt
+++ app/src/main/java/com/example/catsapp/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.catsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var catList = arrayListOf<Cat>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = MainAdapter(this, catList)
+        recyclerView.adapter = adapter
+
     }
 }
